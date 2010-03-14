68395f64f4331ee24685eafdae36518fefe4d1af
zigorou <zigorou@cpan.org>
2010-03-14 21:16:23 +0900

    initial commit

---
+++ lib/XRI/Resolution/Lite.pm
@@ -0,0 +1,33 @@
+package XRI::Resolution::Lite;
+
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+XRI::Resolution::Lite -
+
+=head1 SYNOPSIS
+
+  use XRI::Resolution::Lite;
+
+=head1 DESCRIPTION
+
+XRI::Resolution::Lite is
+
+=head1 AUTHOR
+
+Toru Yamaguchi E<lt>zigorou@cpan.orgE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
